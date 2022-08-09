56ca9748eebbafae78736c6a02ce1a31c3104d13
Mitja Slenc <mslenc@gmail.com>
2022-08-09 23:54:51 +0200

    v0.14.0 - updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,18 +5,18 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.70")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
     }
 }
 
 plugins {
     id("java-library")
-    id("org.jetbrains.kotlin.jvm") version "1.4.20"
+    id("org.jetbrains.kotlin.jvm") version "1.6.21"
     id("maven-publish")
 }
 
 group = "com.github.mslenc"
-version = "0.13.0"
+version = "0.14.0"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
@@ -32,16 +32,16 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.mslenc:utilsktx:1.1.0")
+    implementation("com.github.mslenc:utilsktx:1.3.0")
 
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 
     // https://mvnrepository.com/artifact/io.github.lukehutch/fast-classpath-scanner
     implementation("io.github.lukehutch:fast-classpath-scanner:2.21")
 
-    testImplementation("junit:junit:4.12")
+    testImplementation("junit:junit:4.13.2")
 }
 
 tasks.withType<KotlinCompile> {
